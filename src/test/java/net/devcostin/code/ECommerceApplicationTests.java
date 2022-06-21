c7d899117a9c49809eddfd75cdfd6e17f85efbb1
Constantin Teodorescu <devcostin@gmail.com>
2022-06-21 13:41:12 +0200

    test: second case test failure

--- src/test/java/net/devcostin/code/ECommerceApplicationTests.java
+++ src/test/java/net/devcostin/code/ECommerceApplicationTests.java
@@ -33,4 +33,23 @@ class ECommerceApplicationUnitaryTests {
 		assertThat( FormatDate.formatDateToString(response.getDate()), equalTo("2020-06-14-10.00.00"));
 		assertThat(response.getFinalPrice(), equalTo("35.50EUR"));
 	}
+	
+	@Test
+	void shouldSearchProductPriceSecondCase() {
+		// time:16:00 , day:14, product:35455, brand:1
+		SearchProductPriceUseCase useCase = new SearchProductPriceUseCase();
+		SearchProductPriceResponse response = useCase.execute(
+				SearchProductPriceRequest.builder()
+				.date(LocalDateTime.of(2020, 6, 14, 16, 0, 0))
+				.productId(35455)
+				.brandId(1)
+				.build()
+				);		
+		
+		assertThat(response.productId, equalTo(35455));
+		assertThat(response.brandId, equalTo(1));
+		assertThat(response.getPrice_list(), equalTo(2));
+		assertThat( FormatDate.formatDateToString(response.getDate()), equalTo("2020-06-14-16.00.00"));
+		assertThat(response.getFinalPrice(), equalTo("25.45EUR"));
+	}
 }
